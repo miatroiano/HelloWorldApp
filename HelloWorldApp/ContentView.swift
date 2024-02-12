5c83f596097cfbc85b3af72641a2b99bfa3fb9d3
miatroiano <m11ssing1n4ction@gmail.com>
2024-02-12 08:44:46 -0500

    lightmode

--- HelloWorldApp/ContentView.swift
+++ HelloWorldApp/ContentView.swift
@@ -6,17 +6,53 @@
 //
 
 import SwiftUI
+import UIKit
 
 struct ContentView: View {
+
+    @State private var mode: ColorScheme = .light
+    func toggleMode() {
+            if mode == .light {
+                mode = .dark
+            } else {
+                mode = .light
+            }
+        }
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("testing app")
+            Button("Dark") {
+                
+                self.toggleMode()
+            }
+            .accentColor(/*@START_MENU_TOKEN@*/.orange/*@END_MENU_TOKEN@*/)
+            .font(/*@START_MENU_TOKEN@*/.headline/*@END_MENU_TOKEN@*/)
+            .bold()
+            
+            
+            if(mode == .dark){
+                Image("moon")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+                
+            }
+            else{
+                Image("sun")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+            }
+            
+           
+             
         }
+        
         .padding()
+        .preferredColorScheme(mode)
+        .buttonStyle(.bordered)
+    
     }
+       
 }
 
 #Preview {
