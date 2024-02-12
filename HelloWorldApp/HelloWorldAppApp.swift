3336ae9727090f45161eb50d4bb8a0db6c36366b
miatroiano <m11ssing1n4ction@gmail.com>
2024-02-11 21:26:58 -0500

    Initial Commit

---
+++ HelloWorldApp/HelloWorldAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldAppApp.swift
+//  HelloWorldApp
+//
+//  Created by Mia Troiano on 2/11/24.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
